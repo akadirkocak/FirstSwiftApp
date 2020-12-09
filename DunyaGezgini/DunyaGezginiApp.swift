a42d85163102cd5a3ebdb2347a3555deab2441db
Sam Smithakadirkocak <35079178+akadirkocak@users.noreply.github.com>
2020-12-09 21:08:25 +0100

    Initial Commit

---
+++ DunyaGezgini/DunyaGezginiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DunyaGezginiApp.swift
+//  DunyaGezgini
+//
+//  Created by Abdulkadir Kocak on 09.12.20.
+//
+
+import SwiftUI
+
+@main
+struct DunyaGezginiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
